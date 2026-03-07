7820bc7a396d05426b11caf58d85adc3c82b2a9d
hariharan <hariharansarav7@gmail.com>
2026-03-07 19:03:58 +0530

    Check Palindrome

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
-//1
+//1 Reverse a String
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
 
 console.log(reverseString("hello")); // olleh
 
+//2 Check Palindrome
+
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam")); // true
