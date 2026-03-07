f17b424fb5fc83741741bb1a67a7eb4b77ee0bb0
hariharan <hariharansarav7@gmail.com>
2026-03-07 19:07:14 +0530

    Find Duplicate Numbers in Array

--- javascript.js
+++ javascript.js
@@ -13,3 +13,22 @@ function isPalindrome(str) {
 }
 
 console.log(isPalindrome("madam")); // true
+
+//3 Find Duplicate Numbers in Array
+
+function findduplicate(arr){
+    const seen = new set();
+    const duplicate =[];
+
+    for(let sum of arr){
+        if(seen.has(num)){
+            duplicate.push(num);
+        }else{
+            seen.add(num);
+        }
+    }
+
+    return duplicate;
+}
+
+console.log(findduplicate([1,2,3,4,1,6,7,4,8,]));
